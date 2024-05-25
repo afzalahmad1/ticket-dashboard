2bcb14abdb2d17ab9fbeff0489aa519f95a6685a
Afzal Ahmad <afzalm117@gmail.com>
2024-05-26 00:38:48 +0530

    up

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded';
 
 import "./styles.css";
